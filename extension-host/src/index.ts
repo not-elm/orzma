8a28d2185c657e94cc3c91da737f8e55d3bc74e8
not-elm <elmgameinfo@gmail.com>
2026-05-04 22:04:06 +0900

    add: extension-host minimum code

---
+++ extension-host/src/index.ts
@@ -0,0 +1,30 @@
+import net from "node:net";
+import os from "node:os";
+import path from "node:path";
+
+function getIpcPath(name: string): string {
+  if (process.platform === "win32") {
+    // Windows named pipe
+    return `\\\\.\\pipe\\${name}`;
+  }
+
+  // macOS / Linux Unix domain socket
+  return path.join(os.tmpdir(), `${name}.sock`);
+}
+
+const socketPath = getIpcPath("ozmux-extension-host");
+
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log("received:", data.toString());
+    socket.write(`echo: ${data}`);
+  });
+
+  socket.on("end", () => {
+    console.log("client disconnected");
+  });
+});
+
+server.listen(socketPath, () => {
+  console.log(`listening on ${socketPath}`);
+});
