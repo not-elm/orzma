50c6ad7265b79945a9b2b49344a0fdc070b7614e
not-elm <elmgameinfo@gmail.com>
2026-05-06 19:40:03 +0900

    update: remove temporary

--- sdk/node/src/daemon-uds-client.ts
+++ sdk/node/src/daemon-uds-client.ts
@@ -1,24 +1,24 @@
-import path from "path";
-import os from "node:os";
-import net from "node:net";
-
-export class OzmuxDaemonUdsClient {
-  private constructor(private readonly socket: net.Socket) {}
-
-  static async connect(): Promise<OzmuxDaemonUdsClient> {
-    return new Promise((resolve, reject) => {
-      const socketPath =
-        process.platform === "win32"
-          ? "\\\\.\\pipe\\ozmux-daemon.sock"
-          : path.join(os.tmpdir(), "ozmux-daemon.sock");
-
-      const socket = net.createConnection(socketPath, () => {
-        socket.removeAllListeners("error");
-        resolve(new OzmuxDaemonUdsClient(socket));
-      });
-      socket.once("error", (e) => {
-        reject(e);
-      });
-    });
-  }
-}
+// import path from "path";
+// import os from "node:os";
+// import net from "node:net";
+//
+// export class OzmuxDaemonUdsClient {
+//   private constructor(private readonly socket: net.Socket) {}
+//
+//   static async connect(): Promise<OzmuxDaemonUdsClient> {
+//     return new Promise((resolve, reject) => {
+//       const socketPath =
+//         process.platform === "win32"
+//           ? "\\\\.\\pipe\\ozmux-daemon.sock"
+//           : path.join(os.tmpdir(), "ozmux-daemon.sock");
+//
+//       const socket = net.createConnection(socketPath, () => {
+//         socket.removeAllListeners("error");
+//         resolve(new OzmuxDaemonUdsClient(socket));
+//       });
+//       socket.once("error", (e) => {
+//         reject(e);
+//       });
+//     });
+//   }
+// }
