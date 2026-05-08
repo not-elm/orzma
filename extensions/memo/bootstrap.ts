83fc0c501e59fdee054fe01d642e0cbcd5205282
not-elm <elmgameinfo@gmail.com>
2026-05-08 12:08:28 +0900

    fix: command log

--- extensions/memo/bootstrap.ts
+++ extensions/memo/bootstrap.ts
@@ -5,7 +5,6 @@ import { memoActivity } from "./activities/memo.ts";
 bootstrap({
   commands: {
     memo: async (ctx) => {
-      console.log("DDADAD");
       ctx.stdout.write(`memo invoked in pane ${ctx.pane.paneId}\n`);
       memoActivity.craete({});
       return 0;
