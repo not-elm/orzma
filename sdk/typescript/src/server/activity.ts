fd355eb812ce01728b2345f1c34748dcb294ba32
not-elm <elmgameinfo@gmail.com>
2026-05-10 13:01:21 +0900

    feat(sdk): implement createActivity over daemon HTTP

--- sdk/typescript/src/server/activity.ts
+++ sdk/typescript/src/server/activity.ts
@@ -1,6 +1,14 @@
-interface DefineArgs<T> {
+import * as path from "node:path";
+import { postJson } from "./daemon-client.ts";
+
+export type ActivityId = string;
+
+export interface CreateActivityArgs {
   html: string;
-  initialData: T;
 }
 
-export async function createActivity<T>(_args: DefineArgs<T>) {}
+export async function createActivity(args: CreateActivityArgs): Promise<ActivityId> {
+  const html = path.resolve(args.html);
+  const { activity_id } = await postJson<{ activity_id: ActivityId }>("/activities", { html });
+  return activity_id;
+}
