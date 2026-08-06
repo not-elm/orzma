1f31fc60133a76a75700f540f7dcee74837ecbf0
watanabe <taigawatanabeinfo@gmail.com>
2026-08-06 23:49:38 +0900

    update: impl OrzmaVt for AlacrittyVt

--- crates/orzma_vt/src/vt/alacritty.rs
+++ crates/orzma_vt/src/vt/alacritty.rs
@@ -1,4 +1,4 @@
-use crate::extension::ApcState;
+use crate::{extension::ApcState, vt::OrzmaVt};
 use alacritty_terminal::{
     Grid, Term,
     event::EventListener,
@@ -18,6 +18,28 @@ impl AlacrittyVt {
     pub fn advance(&mut self, bytes: &[u8]) {
         self.apc_parser.parse(bytes, &mut self.apc_state);
         self.processor.advance(&mut self.term, bytes);
+    }
+}
+
+impl OrzmaVt for AlacrittyVt {
+    fn advance(&mut self, chunk: &[u8]) -> crate::prelude::DamageVerdict {
+        todo!()
+    }
+
+    fn frames(&mut self) -> Vec<crate::frame::Frame> {
+        todo!()
+    }
+
+    fn drain_control(&mut self) -> impl Iterator<Item = crate::prelude::ControlFrame> + '_ {
+        todo!()
+    }
+
+    fn drain_replies_into(&self, buf: &mut Vec<u8>) {
+        todo!()
+    }
+
+    fn scroll(&mut self, delta: i32) {
+        todo!()
     }
 }
 
