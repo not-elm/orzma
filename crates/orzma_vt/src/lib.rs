af09dc82f124a072e1a7fbfb7f71c68824889d63
notelm <elmprograminfo@gmail.com>
2026-08-08 18:00:13 +0900

    add: hyperlink.rs

--- crates/orzma_vt/src/lib.rs
+++ crates/orzma_vt/src/lib.rs
@@ -2,10 +2,13 @@ mod cursor;
 mod damage;
 mod extension;
 mod frame;
+mod hyperlink;
 mod modes;
 mod signal;
 mod vt;
 
 pub mod prelude {
-    pub use crate::{cursor::*, damage::DamageVerdict, extension::*, modes::*, signal::*, vt::*};
+    pub use crate::{
+        cursor::*, damage::DamageVerdict, extension::*, hyperlink::*, modes::*, signal::*, vt::*,
+    };
 }
